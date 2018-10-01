2abcef84c15a10211a26d05c4c3c1d15fe2bdef5
Eduardo Mejia <edumejia30@gmail.com>
2018-10-01 09:29:40 -0400

    Correcciones

--- src/config/sales.php
+++ src/config/sales.php
@@ -28,10 +28,8 @@ return [
 	'unibol' => false,
 	'dhl' => false,
 	
-	// CUSTOM FORMS
-    'item_get_after_vars' => ['purchase','product'], // array de nodos: 'node'
-    'item_child_after_vars' => ['product'],
-    'item_remove_scripts' => ['purchase'=>['leave-form']],
-    'item_add_script' => ['purchase'=>['barcode-product'], 'product'=>['product']],
+	// CUSTOM SALES
+    'custom_add_cart' => false, // Reglas de revisión al añadir producto personalizadas
+    'custom_add_cart_detail' => false, // DEtalle extra para añadir al carro de compras
 
 ];
