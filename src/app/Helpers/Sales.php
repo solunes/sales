bc7a3b7f8c3856e9f63132ccd6e65127159775f5
Eduardo Mejia <edumejia30@gmail.com>
2020-05-11 12:29:03 -0400

    fix

--- src/app/Helpers/Sales.php
+++ src/app/Helpers/Sales.php
@@ -282,36 +282,40 @@ class Sales {
 
   public static function calculate_shipping_cost($shipping_id, $country_id, $city_id, $weight) {
     $shipping = \Solunes\Sales\App\Shipping::find($shipping_id);
-    if(config('sales.delivery_country')){
-      $country = \Solunes\Business\App\Country::find($country_id);
-      $shipping_cities = $shipping->shipping_cities()->where('country_id', $country_id)->with('city')->get();
-      $shipping_city = $shipping->shipping_cities()->where('country_id', $country_id)->where('city_id', $city_id)->first();
-      if(!$shipping_city){
-        $shipping_city = $shipping->shipping_cities()->where('country_id', $country_id)->first();
-        $city_id = $shipping_city->city_id;
-      }
+    if($shipping->script){
+      return \CustomFunc::calculate_shipping_cost($shipping, $country_id, $city_id, $weight);
     } else {
-      $shipping_cities = $shipping->shipping_cities()->with('city')->get();
-      $shipping_city = $shipping->shipping_cities()->where('city_id', $city_id)->first();
-    }
-    $shipping_cities_array = [];
-    foreach($shipping_cities as $shipping_city_item){
-      $shipping_cities_array[$shipping_city_item->city->name] = $shipping_city_item->city_id;
-    }
-    $other_city = false;
-    if($shipping_city){
-      $shipping_cost = $shipping_city->shipping_cost;
-      if($shipping_city->city->other_city){
-        $other_city = true;
-      }
-      $weight = $weight-1;
-      if($weight>0){
-          $shipping_cost += ceil($weight)*$shipping_city->shipping_cost_extra;
-      }
-      return ['shipping'=>true, 'shipping_cities'=>$shipping_cities_array, 'shipping_city'=>$shipping_city->city_id, 'other_city'=>$other_city, 'shipping_cost'=>$shipping_cost];
-    } else {
-      $new_shipping_id = 2;
-      return ['shipping'=>false,  'shipping_cities'=>$shipping_cities_array, 'shipping_city'=>1, 'other_city'=>$other_city, 'shipping_cost'=>0, 'new_shipping_id'=>$new_shipping_id];
+      if(config('sales.delivery_country')){
+          $country = \Solunes\Business\App\Country::find($country_id);
+          $shipping_cities = $shipping->shipping_cities()->where('country_id', $country_id)->with('city')->get();
+          $shipping_city = $shipping->shipping_cities()->where('country_id', $country_id)->where('city_id', $city_id)->first();
+          if(!$shipping_city){
+            $shipping_city = $shipping->shipping_cities()->where('country_id', $country_id)->first();
+            $city_id = $shipping_city->city_id;
+          }
+        } else {
+          $shipping_cities = $shipping->shipping_cities()->with('city')->get();
+          $shipping_city = $shipping->shipping_cities()->where('city_id', $city_id)->first();
+        }
+        $shipping_cities_array = [];
+        foreach($shipping_cities as $shipping_city_item){
+          $shipping_cities_array[$shipping_city_item->city->name] = $shipping_city_item->city_id;
+        }
+        $other_city = false;
+        if($shipping_city){
+          $shipping_cost = $shipping_city->shipping_cost;
+          if($shipping_city->city->other_city){
+            $other_city = true;
+          }
+          $weight = $weight-1;
+          if($weight>0){
+              $shipping_cost += ceil($weight)*$shipping_city->shipping_cost_extra;
+          }
+          return ['shipping'=>true, 'shipping_cities'=>$shipping_cities_array, 'shipping_city'=>$shipping_city->city_id, 'other_city'=>$other_city, 'shipping_cost'=>$shipping_cost];
+        } else {
+          $new_shipping_id = 2;
+          return ['shipping'=>false,  'shipping_cities'=>$shipping_cities_array, 'shipping_city'=>1, 'other_city'=>$other_city, 'shipping_cost'=>0, 'new_shipping_id'=>$new_shipping_id];
+        }
     }
   }
 
